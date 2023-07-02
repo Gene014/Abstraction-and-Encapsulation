c1e150ee6ffbe8f3ef22b03ea248395f2e095c89
Gene014 <calmaeugene06@gmail.com>
2023-07-02 12:47:00 +0800

    copying pseudocode and importing fan functions to test fan class.

---
+++ Fan/test_fan.py
@@ -0,0 +1,8 @@
+#Calma, Eugene Marie S.
+
+#import fanfunc
+from fan_functions import Fan
+# test fan
+# For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
+# Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
+
