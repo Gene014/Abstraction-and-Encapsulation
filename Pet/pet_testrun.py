dac97d372f0be74630e1dad47a0c806865324b65
Gene014 <calmaeugene06@gmail.com>
2023-07-02 22:55:34 +0800

    adding some colors

--- Pet/pet_testrun.py
+++ Pet/pet_testrun.py
@@ -4,11 +4,11 @@ from pet_class import Pet
 #create testrun
 class PetTest:
     def userint():
-        name = input("Good Day! Please Enter the name of your Pet.")
-        animal_type = input ("Please Enter the type or breed of your Pet.")
-        age = input ("Please Enter the age of your Pet.")
+        name = input("\33[33mGood Day! Please Enter the name of your Pet.")
+        animal_type = input ("\33[35mPlease Enter the type or breed of your Pet.")
+        age = input ("\33[34mPlease Enter the age of your Pet.")
         pet = Pet(name, animal_type, age)
 #print pet informations
-        print(f'Your Pet name is{pet.get_name()}.')
-        print(f'Your pet is {pet.get_animal_type()}.')
-        print(f'The age of your pet is {pet.get_age()} years old.')      
+        print(f'\n\33[33mYour Pet name is{pet.get_name()}.')
+        print(f'\n\33[35mYour pet is {pet.get_animal_type()}.')
+        print(f'\n\33[34mThe age of your pet is {pet.get_age()} years old.')      
