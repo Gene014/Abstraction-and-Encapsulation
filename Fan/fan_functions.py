5ef883266dc9b2459991aa7781f09f08ecbaa1f3
Gene014 <calmaeugene06@gmail.com>
2023-07-02 12:46:22 +0800

    copying pseudocode and importing fan functions to test fan class.

--- Fan/fan_functions.py
+++ Fan/fan_functions.py
@@ -1,3 +1,4 @@
+#Calma, Eugene Marie S.
 #(The Fan class).  Design a class named Fan to represent a fan. The class contains:
 # Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
 # ■ A private int data field named speed that specifies the speed of the fan.
